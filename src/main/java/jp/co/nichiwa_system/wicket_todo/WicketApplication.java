0dde46697206b26739f3e6d6de96bc459a31752b
Y.Okumura <y-okumura@nichiwa-system.co.jp>
2015-03-23 09:05:15 +0900

    mvn archetype:generate のインタラクティブモードでwicketプロジェクトを生成した直後。versionは最新の7.0.0-M5を使用。

---
+++ src/main/java/jp/co/nichiwa_system/wicket_todo/WicketApplication.java
@@ -0,0 +1,33 @@
+package jp.co.nichiwa_system.wicket_todo;
+
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application.
+ * If you want to run this application without deploying, run the Start class.
+ * 
+ * @see jp.co.nichiwa_system.wicket_todo.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}
+}
