3eded24bcd300990984a9d63dcdd4050b12b4d6f
Y.Okumura <y-okumura@nichiwa-system.co.jp>
2015-04-03 02:04:18 +0900

    todoに作り変え

--- src/main/java/jp/co/nichiwa_system/wicket_todo/HomePage.java
+++ src/main/java/jp/co/nichiwa_system/wicket_todo/HomePage.java
@@ -1,18 +1,55 @@
 package jp.co.nichiwa_system.wicket_todo;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
+import org.apache.wicket.datetime.markup.html.basic.DateLabel;
+import org.apache.wicket.datetime.markup.html.form.DateTextField;
+import org.apache.wicket.extensions.yui.calendar.DatePicker;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.PropertyModel;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
+		
+		add(
+			new Form<TodoList>("todoList", new CompoundPropertyModel<>(new TodoList())) {
+				@Override
+				protected void onSubmit() {
+					getModelObject().newTask();
+				}
+			}.add(
+				new ListView<Todo>("list") {
+					@Override
+					protected void populateItem(ListItem<Todo> li) {
+						Todo todo = li.getModelObject();
+						li.add(
+							new CheckBox("done", new PropertyModel(todo, "done")),
+							new Label("description", new PropertyModel(todo, "description")),
+							DateLabel.forShortStyle("due", new PropertyModel(todo, "due"))
+						);
+					}
+				},
+				new TextField<String>("description").setRequired(true),
+				DateTextField.forShortStyle("due").add(new DatePicker().setShowOnFieldClick(true).setAutoHide(true)),
+				new AjaxButton("add") {
+					@Override
+					protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+						target.add(form);
+					}
+				}
+			).setOutputMarkupId(true)
+		);
 
     }
 }
---
+++ src/main/java/jp/co/nichiwa_system/wicket_todo/TodoList.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2015 okumura.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.co.nichiwa_system.wicket_todo;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author okumura
+ */
+class TodoList implements Serializable {
+
+	private final List<Todo> list = new ArrayList<>();
+	private String description = "";
+	private Date due;
+	
+	public TodoList() {
+	}
+
+	void newTask() {
+		list.add(new Todo(description, due));
+		description = "";
+		due = null;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDue() {
+		return due;
+	}
+
+	public void setDue(Date due) {
+		this.due = due;
+	}
+	
+	public List<Todo> getList() {
+		return Collections.unmodifiableList(list);
+	}
+}
+
+/**
+ *
+ * @author okumura
+ */
+class Todo implements Serializable {
+	private boolean done;
+	private String description;
+	private Date due;
+	
+	public Todo(String description, Date due) {
+		this.description = description;
+		this.due = due;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getDue() {
+		return due;
+	}
+
+	public void setDue(Date due) {
+		this.due = due;
+	}
+}
